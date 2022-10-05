133cb7cc4aba72b98cc9d87891c6df4306b20237
M Mehrtens <michael.david.mehrtens@gmail.com>
2022-10-05 16:51:27 -0500

    Update Kinesis for flush data

--- source/demo/send-messages.sh
+++ source/demo/send-messages.sh
@@ -40,26 +40,18 @@ done
 for (( i = 1; i <= $ITERATIONS; i++)) {
 
   DEVICE="P0"$((1 + $RANDOM % 5))
-  FLOW=$(( 60 + $RANDOM % 40 ))
-  TEMP=$(( 15 + $RANDOM % 20 ))
-  HUMIDITY=$(( 50 + $RANDOM % 40 ))
-  SOUND=$(( 100 + $RANDOM % 40 ))
-
-  # 3% chance of throwing an anomalous temperature reading
-  if [ $(($RANDOM % 100)) -gt 97 ]
-  then
-    echo "TEMPERATURE OUT OF RANGE"
-    TEMP=$(($TEMP*6))
-  fi
 
   echo "Publishing message $i/$ITERATIONS to IoT topic $TOPIC:"
   echo "device: $DEVICE"
-  echo "flow: $FLOW"
-  echo "temp: $TEMP"
-  echo "humidity: $HUMIDITY"
-  echo "sound: $SOUND"
 
-  aws iot-data publish --topic "$TOPIC" --payload "{\"id\":\"1\",\"device\":\"$DEVICE\",\"flow\":$FLOW,\"temp\":$TEMP,\"humidity\":$HUMIDITY,\"sound\":$SOUND}" --profile "$PROFILE" --region "$REGION"
+  aws iot-data publish \
+    --topic "$TOPIC" \
+    --profile "$PROFILE" \
+    --region "$REGION" \
+    --payload '{'
+      '"device_id": "' $DEVICE '", '
+      '"ts": "' $(date +%s) '",'
+      '}'
 
   sleep $WAIT
 
